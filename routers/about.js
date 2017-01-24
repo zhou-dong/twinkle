bcb53255ba1b4e11d8de5b3ea59bdc39228be72d
zhou-dong <82224165@qq.com>
2017-01-23 21:11:01 -0600

    add highlight js

--- routers/about.js
+++ routers/about.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
+var appDir = path.dirname(require.main.filename);
+var configDir = path.join(appDir, "config");
+var projectConfig = require(configDir + "/project.config");
 
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
@@ -11,8 +16,10 @@ router.get('/', function(req, res) {
 	res.send('about home page');
 });
 
+console.log(projectConfig);
+
 router.get('/about', function(req, res) {
-	res.send('about about page');
+	res.render('pages/about', projectConfig);
 });
 
 module.exports = router
